3f1e2fac9015e1def1e29a079d415fe89a370815
Jonathan Gravel <jo@stashed.dev>
2025-11-21 22:48:41 -0500

    neovim: format lua files

--- config/nvim/lua/yobiscus/plugins/completion.lua
+++ config/nvim/lua/yobiscus/plugins/completion.lua
@@ -1,83 +1,83 @@
 return {
-    {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = {
-      'Kaiser-Yang/blink-cmp-avante',
-      'rafamadriz/friendly-snippets',
-    },
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = {
+			"Kaiser-Yang/blink-cmp-avante",
+			"rafamadriz/friendly-snippets",
+		},
 
-    -- use a release tag to download pre-built binaries
-    version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "default" },
 
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
 
-      cmdline = {
-        keymap = {
-          preset = 'cmdline',
-          ['<Tab>'] = { 'show_and_insert', 'select_and_accept' }
-        }
-      },
+			cmdline = {
+				keymap = {
+					preset = "cmdline",
+					["<Tab>"] = { "show_and_insert", "select_and_accept" },
+				},
+			},
 
-      -- Show documentation by default, show borders around text
-      completion = {
-        menu = {
-          border = "rounded",
-        },
-        documentation = {
-          auto_show = true,
-          window = { border = "rounded" },
-        },
-      },
+			-- Show documentation by default, show borders around text
+			completion = {
+				menu = {
+					border = "rounded",
+				},
+				documentation = {
+					auto_show = true,
+					window = { border = "rounded" },
+				},
+			},
 
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        -- Add 'avante' to the list
-        default = { 'avante', 'lsp', 'path', 'snippets', 'buffer' },
-        providers = {
-          avante = {
-            module = 'blink-cmp-avante',
-            name = 'Avante',
-            opts = {
-              -- options for blink-cmp-avante
-            }
-          }
-        },
-      },
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				-- Add 'avante' to the list
+				default = { "avante", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					avante = {
+						module = "blink-cmp-avante",
+						name = "Avante",
+						opts = {
+							-- options for blink-cmp-avante
+						},
+					},
+				},
+			},
 
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
-    opts_extend = { "sources.default" }
-  }
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
 }
--- config/nvim/lua/yobiscus/plugins/format.lua
+++ config/nvim/lua/yobiscus/plugins/format.lua
@@ -1,34 +1,34 @@
 return {
-  'stevearc/conform.nvim',
-  lazy = false,
-  keys = {
-    {
-      '<leader>lf',
-      function()
-        require('conform').format { async = true, lsp_fallback = true }
-      end,
-      mode = 'n',
-      desc = 'format buffer',
-    },
-  },
-  opts = {
-    format_on_save = true,
-    formatters_by_ft = {
-      css = { 'prettier' },
-      html = { 'prettier' },
-      htmldjango = { 'prettier' },
-      json = { 'jq' },
-      jsonc = { 'jq' },
-      lua = { 'stylua' },
-      markdown = { "markdown-toc" },
-      python = function(bufnr)
-        if require("conform").get_formatter_info("ruff_format", bufnr).available then
-          return { "ruff_format" }
-        else
-          return { "isort", "black" }
-        end
-      end,
-      rust = { "rustfmt", "leptosfmt", lsp_format = "fallback" },
-    },
-  }
+	"stevearc/conform.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader>lf",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "n",
+			desc = "format buffer",
+		},
+	},
+	opts = {
+		format_on_save = true,
+		formatters_by_ft = {
+			css = { "prettier" },
+			html = { "prettier" },
+			htmldjango = { "prettier" },
+			json = { "jq" },
+			jsonc = { "jq" },
+			lua = { "stylua" },
+			markdown = { "markdown-toc" },
+			python = function(bufnr)
+				if require("conform").get_formatter_info("ruff_format", bufnr).available then
+					return { "ruff_format" }
+				else
+					return { "isort", "black" }
+				end
+			end,
+			rust = { "rustfmt", "leptosfmt", lsp_format = "fallback" },
+		},
+	},
 }
