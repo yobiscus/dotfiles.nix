55c6ad47305e333f2506eae3f00b5aed7865635c
Jonathan Gravel <jo@stashed.dev>
2025-12-10 21:52:58 -0500

    neovim: make background transparent (copy from ml4w)

--- config/nvim/lua/yobiscus/init.lua
+++ config/nvim/lua/yobiscus/init.lua
@@ -33,5 +33,18 @@ vim.keymap.set("n", "<leader>eg", function()
 	vim.cmd("e %:r." .. ext)
 end, { desc = "guess" })
 
+--
+vim.cmd([[
+hi NonText ctermbg=none guibg=NONE
+hi Normal guibg=NONE ctermbg=NONE
+hi NormalNC guibg=NONE ctermbg=NONE
+hi SignColumn ctermbg=NONE ctermfg=NONE guibg=NONE
+
+hi Pmenu ctermbg=NONE ctermfg=NONE guibg=NONE
+hi FloatBorder ctermbg=NONE ctermfg=NONE guibg=NONE
+hi NormalFloat ctermbg=NONE ctermfg=NONE guibg=NONE
+hi TabLine ctermbg=None ctermfg=None guibg=None
+]])
+
 -- plugins
 require("yobiscus.lazy")
