b774fc83729bb2cf3eea20b1d66afc3604fe8841
Jonathan Gravel <jo@stashed.dev>
2025-12-23 15:02:58 -0500

    tmux: use matugen color palette

--- config/home-manager/modules/terminal/tmux.nix
+++ config/home-manager/modules/terminal/tmux.nix
@@ -5,5 +5,14 @@
     pkgs.tmux
   ];
 
-  home.file.".config/tmux".source = ../../../tmux;
+  home.file = {
+    ".config/tmux/tmux.conf".source = ../../../tmux/tmux.conf;
+    ".config/tmux/theme.conf".source = ../../../tmux/theme.conf;
+    # ".config/tmux/theme.conf".source =
+    #   config.lib.file.mkOutOfStoreSymlink
+    #   "${config.home.homeDirectory}/.dotfiles/config/tmux/theme.conf";
+    ".config/tmux/colors-matugen.conf".source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/config/tmux/colors-matugen.conf";
+  };
 }
