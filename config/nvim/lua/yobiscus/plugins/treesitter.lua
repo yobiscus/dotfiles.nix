82bc342f8c276de7da9966f672066c1b9f444d08
Jonathan Gravel <jo@stashed.dev>
2026-02-10 11:09:26 -0500

    neovim: fix treesitter config

--- config/nvim/lua/yobiscus/plugins/treesitter.lua
+++ config/nvim/lua/yobiscus/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter.config").setup({
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
 				ensure_installed = {
 					"c",
