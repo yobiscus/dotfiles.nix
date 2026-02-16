f41177b4410c791b078df2a869f620139388ff75
Jonathan Gravel <jo@stashed.dev>
2026-02-15 22:53:30 -0500

    nvim: configure keybinds for gitsigns, uninstall vim-fugitive

--- config/nvim/lua/yobiscus/plugins/git.lua
+++ config/nvim/lua/yobiscus/plugins/git.lua
@@ -1,11 +1,29 @@
 return {
-	"https://github.com/tpope/vim-fugitive",
 	{
 		"https://github.com/lewis6991/gitsigns.nvim",
 		config = function()
 			local gitsigns = require("gitsigns")
 			gitsigns.setup()
-			vim.keymap.set("n", "<leader>gb", gitsigns.toggle_current_line_blame, { desc = "Toggle Blame" })
+
+			-- modes
+			vim.keymap.set("n", "<leader>gb", gitsigns.blame, { desc = "Toggle blame" })
+			vim.keymap.set("n", "<leader>gv", gitsigns.preview_hunk, { desc = "Toggle hunk preview" })
+
+			-- mutation
+			vim.keymap.set("n", "<leader>gs", function()
+				gitsigns.stage_hunk(nil, { greedy = false })
+			end, { desc = "Stage hunk" })
+			vim.keymap.set("v", "<leader>gs", function()
+				gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }, { greedy = false })
+			end, { desc = "Stage hunk (visual)" })
+
+			-- navigation
+			vim.keymap.set("n", "<leader>gn", function()
+				gitsigns.nav_hunk("next", { greedy = false })
+			end, { desc = "Next hunk" })
+			vim.keymap.set("n", "<leader>gp", function()
+				gitsigns.nav_hunk("prev", { greedy = false })
+			end, { desc = "Prev hunk" })
 		end,
 	},
 }
