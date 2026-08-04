6a0fc47c53e5b2099f0a8f6ba992f9d71ca300a5
Jonathan Gravel <jo@stashed.dev>
2026-08-04 12:06:15 -0400

    tmux: install tmux-fingers plugin

--- config/home-manager/modules/terminal/tmux.nix
+++ config/home-manager/modules/terminal/tmux.nix
@@ -5,11 +5,7 @@
     pkgs.tmux
   ];
 
-  home.file = {
-    ".config/tmux/tmux.conf".source = ../../../tmux/tmux.conf;
-    ".config/tmux/theme.conf".source = ../../../tmux/theme.conf;
-    ".config/tmux/colors-matugen.conf".source =
-      config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.dotfiles/config/tmux/colors-matugen.conf";
-  };
+  home.file.".config/tmux".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.dotfiles/config/tmux";
 }
